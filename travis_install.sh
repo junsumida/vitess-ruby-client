9b97b8e675129db4c06ff32daa7d28aeaaf78feb
Jun Sumida <jun.sumida@mixi.co.jp>
2016-01-21 12:54:00 +0900

    fix

--- travis_install.sh
+++ travis_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+    #!/bin/bash
 #
 # Builds and installs gRPC on a box (from sources).
 #
@@ -6,6 +6,21 @@
 # build our development environment. This script does that. It is intended
 # to run from file .travis.yml as root on Ubuntu 12.04 LTS.
 
+# before compiling
+apt-get install -y build-essential libtool
+
+# install autoconf from source code
+git clone http://git.sv.gnu.org/r/autoconf.git
+cd autoconf
+aclocal -I m4
+automake -a
+autoconf
+./configure --prefix=/usr
+make
+sudo make install
+
+# install grpc
+cd ..
 mkdir pre_build
 cd pre_build
 git clone https://github.com/grpc/grpc.git
@@ -13,6 +28,4 @@ cd grpc
 # Currently we can build against this gRPC label.
 #git checkout 4831d02cc2341ec2233ff9d9ef66fb9a86138fb7
 git submodule update --init
-apt-get install -y build-essential autoconf libtool
 make
-make install prefix=/opt
